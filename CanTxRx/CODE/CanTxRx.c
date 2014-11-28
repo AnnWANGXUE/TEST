590593d7e62310eec6dc7cfb750cd3f18ce0dddb
AnnWANGXUE <annwang@hkpc.org>
2014-11-28 12:06:09 +0800

    first

---
+++ CanTxRx/CODE/CanTxRx.c
@@ -0,0 +1,59 @@
+/** ###################################################################
+**     Filename  : CanTxRx.C
+**     Project   : CanTxRx
+**     Processor : MC9S12DP512BMPV
+**     Version   : Driver 01.13
+**     Compiler  : CodeWarrior HC12 C Compiler
+**     Date/Time : 7/12/2012, 17:45
+**     Abstract  :
+**         Main module.
+**         This module contains user's application code.
+**     Settings  :
+**     Contents  :
+**         No public methods
+**
+**     (c) Copyright UNIS, a.s. 1997-2008
+**     UNIS, a.s.
+**     Jundrovska 33
+**     624 00 Brno
+**     Czech Republic
+**     http      : www.processorexpert.com
+**     mail      : info@processorexpert.com
+** ###################################################################*/
+/* MODULE CanTxRx */
+
+/* Including needed modules to compile this module/procedure */
+#include "Cpu.h"
+#include "Events.h"
+/* Include shared modules, which are used for whole project */
+#include "PE_Types.h"
+#include "PE_Error.h"
+#include "PE_Const.h"
+#include "IO_Map.h"
+
+void main(void)
+{
+  void _Startup(void);
+  
+  /* Write your local variable definition here */
+
+  /*** Processor Expert internal initialization. DON'T REMOVE THIS CODE!!! ***/
+  PE_low_level_init();
+  /*** End of Processor Expert internal initialization.                    ***/
+
+  /* Write your code here */
+
+  /*** Processor Expert end of main routine. DON'T MODIFY THIS CODE!!! ***/
+  for(;;){}
+  /*** Processor Expert end of main routine. DON'T WRITE CODE BELOW!!! ***/
+} /*** End of main routine. DO NOT MODIFY THIS TEXT!!! ***/
+
+/* END CanTxRx */
+/*
+** ###################################################################
+**
+**     This file was created by UNIS Processor Expert 3.00 [04.12]
+**     for the Freescale HCS12 series of microcontrollers.
+**
+** ###################################################################
+*/
---
+++ CanTxRx/CODE/Events.h
@@ -0,0 +1,47 @@
+/** ###################################################################
+**     Filename  : Events.H
+**     Project   : CanTxRx
+**     Processor : MC9S12DP512BMPV
+**     Beantype  : Events
+**     Version   : Driver 01.04
+**     Compiler  : CodeWarrior HC12 C Compiler
+**     Date/Time : 7/12/2012, 17:45
+**     Abstract  :
+**         This is user's event module.
+**         Put your event handler code here.
+**     Settings  :
+**     Contents  :
+**         No public methods
+**
+**     (c) Copyright UNIS, a.s. 1997-2008
+**     UNIS, a.s.
+**     Jundrovska 33
+**     624 00 Brno
+**     Czech Republic
+**     http      : www.processorexpert.com
+**     mail      : info@processorexpert.com
+** ###################################################################*/
+
+#ifndef __Events_H
+#define __Events_H
+/* MODULE Events */
+
+#include "PE_Types.h"
+#include "PE_Error.h"
+#include "PE_Const.h"
+#include "IO_Map.h"
+
+#pragma CODE_SEG DEFAULT
+
+
+/* END Events */
+#endif /* __Events_H*/
+
+/*
+** ###################################################################
+**
+**     This file was created by UNIS Processor Expert 3.00 [04.12]
+**     for the Freescale HCS12 series of microcontrollers.
+**
+** ###################################################################
+*/
